cf03b548f3ab7f603f0b7d854bcde455525b5a16
Berkay Sazak <berkaysazak313@gmail.com>
2022-10-21 19:50:28 +0300

    Fragment

--- app/src/main/java/com/example/movieapp/view/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.movieapp
+package com.example.movieapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.movieapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/movieapp/view/MovieFragment.kt
@@ -0,0 +1,7 @@
+package com.example.movieapp.view
+
+import androidx.fragment.app.Fragment
+import com.example.movieapp.R
+
+class MovieFragment : Fragment(R.layout.fragment_movies) {
+}
